45dfa51eac728af6e93afdffee327d67bdc4259f
Ives van der Flaas <ives.vdf@gmail.com>
2011-02-08 22:21:35 +0100

    Used jgoodies on the instance info input page in the new server wizard

--- src/be/ac/ua/comp/scarletnebula/gui/addserverwizard/InstanceInformationPage.java
+++ src/be/ac/ua/comp/scarletnebula/gui/addserverwizard/InstanceInformationPage.java
@@ -1,21 +1,20 @@
 package be.ac.ua.comp.scarletnebula.gui.addserverwizard;
 
-import java.awt.Dimension;
 import java.util.Collection;
 
 import javax.swing.JComboBox;
-import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 import be.ac.ua.comp.scarletnebula.core.CloudProvider;
 import be.ac.ua.comp.scarletnebula.wizard.DataRecorder;
 import be.ac.ua.comp.scarletnebula.wizard.WizardPage;
 
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.FormLayout;
+
 class InstanceInformationPage extends WizardPage
 {
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 	final JTextField instanceNameField = new JTextField();
 	final JComboBox instanceSizeList = new JComboBox();
@@ -25,17 +24,24 @@ class InstanceInformationPage extends WizardPage
 		instanceNameField.setText(provider.getName() + "-"
 				+ provider.listLinkedServers().size());
 		instanceNameField.selectAll();
-		instanceNameField.setPreferredSize(new Dimension(100, 25));
-		add(new JLabel("Instance name:"));
-		add(instanceNameField);
 
-		add(new JLabel("Instance size:"));
 		Collection<String> sizes = provider.getPossibleInstanceSizes();
 		for (String size : sizes)
 			instanceSizeList.addItem(size);
 
 		instanceSizeList.setSelectedIndex(0);
-		add(instanceSizeList);
+
+		FormLayout layout = new FormLayout(
+				"right:max(40dlu;p), 4dlu, max(50dlu;p):grow, 7dlu", "");
+
+		DefaultFormBuilder builder = new DefaultFormBuilder(layout);
+		builder.setDefaultDialogBorder();
+
+		builder.append("Instance name", instanceNameField);
+		builder.nextLine();
+		builder.append("Instance size", instanceSizeList);
+
+		add(builder.getPanel());
 	}
 
 	@Override
