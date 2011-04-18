dae5f90677bb299e6b9f5c91962190cda3b6e24a
Ives van der Flaas <ives.vdf@gmail.com>
2011-04-19 01:51:23 +0200

    Some basic graph stuff

---
+++ src/be/ac/ua/comp/scarletnebula/gui/graph/Datapoint.java
@@ -0,0 +1,50 @@
+package be.ac.ua.comp.scarletnebula.gui.graph;
+
+public class Datapoint
+{
+	public enum Type
+	{
+		RELATIVE, ABSOLUTE
+	};
+
+	final Type datapointType;
+	final String datastream;
+	final double value;
+	final double lowWarnLevel;
+	final double mediumWarnLevel;
+	final double highWarnLevel;
+
+	public Datapoint(Type datapointType, String datastream, double value,
+			double lowWarnLevel, double mediumWarnLevel, double highWarnLevel)
+	{
+		this.datapointType = datapointType;
+		this.datastream = datastream;
+		this.value = value;
+		this.lowWarnLevel = lowWarnLevel;
+		this.mediumWarnLevel = mediumWarnLevel;
+		this.highWarnLevel = highWarnLevel;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (!(obj instanceof Datapoint))
+		{
+			return false;
+		}
+		Datapoint o = (Datapoint) obj;
+		return this.datapointType == o.datapointType
+				&& this.datastream.equals(o.datastream)
+				&& this.value == o.value && this.lowWarnLevel == o.lowWarnLevel
+				&& this.mediumWarnLevel == o.mediumWarnLevel
+				&& this.highWarnLevel == o.highWarnLevel;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "[" + datapointType + " in " + datastream + "] " + value
+				+ " (WL=" + lowWarnLevel + ", WM=" + mediumWarnLevel + ", WH="
+				+ highWarnLevel + ")";
+	}
+}
---
+++ src/be/ac/ua/comp/scarletnebula/gui/graph/DatapointSerialisationTest.java
@@ -0,0 +1,46 @@
+package be.ac.ua.comp.scarletnebula.gui.graph;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.google.gson.Gson;
+
+public class DatapointSerialisationTest
+{
+	@Test
+	public void serialiseTest()
+	{
+		Gson gson = new Gson();
+		Datapoint dp = getTestDataPoints();
+		Datapoint[] dps = { dp, dp, dp };
+		// System.out.println(gson.toJson(dps));
+		assertEquals(gson.toJson(dps), getSerialisedDatapoints());
+	}
+
+	private Datapoint getTestDataPoints()
+	{
+		Datapoint dp = new Datapoint(Datapoint.Type.RELATIVE, "CPU", 0.63, // value
+				0.5, // low Warning
+				0.85, // medium warning
+				0.95); // high warning
+		return dp;
+	}
+
+	@Test
+	public void deserialiseTest()
+	{
+		Gson gson = new Gson();
+		Datapoint[] dps = gson.fromJson(getSerialisedDatapoints(),
+				Datapoint[].class);
+
+		assertEquals(dps.length, 3);
+		for (Datapoint dp : dps)
+			assertEquals(getTestDataPoints(), dp);
+	}
+
+	private String getSerialisedDatapoints()
+	{
+		return "[{\"datapointType\":\"RELATIVE\",\"datastream\":\"CPU\",\"value\":0.63,\"lowWarnLevel\":0.5,\"mediumWarnLevel\":0.85,\"highWarnLevel\":0.95},{\"datapointType\":\"RELATIVE\",\"datastream\":\"CPU\",\"value\":0.63,\"lowWarnLevel\":0.5,\"mediumWarnLevel\":0.85,\"highWarnLevel\":0.95},{\"datapointType\":\"RELATIVE\",\"datastream\":\"CPU\",\"value\":0.63,\"lowWarnLevel\":0.5,\"mediumWarnLevel\":0.85,\"highWarnLevel\":0.95}]";
+	}
+}
