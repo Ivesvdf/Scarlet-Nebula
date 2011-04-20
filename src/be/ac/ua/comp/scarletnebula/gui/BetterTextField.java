ba25f20b41b61416567491eb307aa02de47a43f0
Ives van der Flaas <ives.vdf@gmail.com>
2011-04-21 00:24:47 +0200

    More comments

--- src/be/ac/ua/comp/scarletnebula/gui/BetterTextField.java
+++ src/be/ac/ua/comp/scarletnebula/gui/BetterTextField.java
@@ -8,32 +8,62 @@ import javax.swing.JTextField;
 
 import be.ac.ua.comp.scarletnebula.misc.Colors;
 
+/**
+ * A derivative of a JTextField that provides extra functionality such as easy
+ * placeholders.
+ * 
+ * @author ives
+ */
 public class BetterTextField extends JTextField
 {
 	private static final long serialVersionUID = 1L;
 	private boolean originalPlaceholderText = true;
+	private boolean placeholderDisplayed = false;
 
+	/**
+	 * @see JTextField
+	 */
 	public BetterTextField(int columns)
 	{
 		super(columns);
 	}
 
+	/**
+	 * @see JTextField
+	 */
 	public BetterTextField()
 	{
 		super();
 	}
 
-	public void setPlaceHolder(String placeholder)
+	/**
+	 * Enables the JTextField to have a placeholder and sets the initial text to
+	 * placeholder. Call this somewhere before showing the textfield.
+	 * 
+	 * Concretly this placeholder will take the shape of gray text that will
+	 * appear when the field contains no text and is not focussed and that will
+	 * disappear when the field receives focus.
+	 * 
+	 * @param placeholder
+	 */
+	public void setPlaceHolder(final String placeholder)
 	{
 		setText(placeholder);
 		final Color originalTextColor = getForeground();
-		setForeground(Colors.Gray.alpha(1.0f));
+		setForeground(Colors.Gray.color());
+		placeholderDisplayed = true;
+
 		addFocusListener(new FocusListener()
 		{
 			@Override
 			public void focusLost(FocusEvent e)
 			{
-
+				if (getText().length() == 0)
+				{
+					setForeground(Colors.Gray.alpha(1.0f));
+					setText(placeholder);
+					placeholderDisplayed = true;
+				}
 			}
 
 			@Override
@@ -44,8 +74,26 @@ public class BetterTextField extends JTextField
 					originalPlaceholderText = false;
 					setForeground(originalTextColor);
 					setText("");
+					placeholderDisplayed = false;
 				}
 			}
 		});
+	}
+
+	/**
+	 * @return The field's content if there is any, the empty string if the
+	 *         BetterTextField has a placeholder displayed.
+	 */
+	@Override
+	public String getText()
+	{
+		String result = null;
+
+		if (placeholderDisplayed)
+			result = "";
+		else
+			result = super.getText();
+
+		return result;
 	}
 }
